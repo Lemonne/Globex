dd8812a5be2cd35e64432ce26672447c77240844
Nerissa Lemon <Nerissa@Nerissas-MacBook-Pro.local>
2015-11-30 20:27:12 -0800

    making changes to viz.js, still broken

--- public/viz.js
+++ public/viz.js
@@ -77,6 +77,7 @@
  * @author: Nerissa Lemon
  * @since: Jan 6, 2015
  */
+var libs = libs || {};
 google.load('visualization', '1', {packages: ['corechart']});
 
 google.setOnLoadCallback(vizInit);
@@ -87,90 +88,93 @@ var views = {};
 var totals = {};
 // Define the variable to hold the chart.
 var chart;
-    // Store the data by creating a google DataTable object with
-    // two columns: Month and People Hours.
-    //data.addColumn('string', 'Course Level');
-    //data.addColumn('number', 'Number of Students');
 
-    // Add 12 rows to the DataTable, January - December of
-    // 2014.
-    // data.addRows([
-    //     ['100', 1760],
-    //     ['200', 2120],
-    //     ['300', 698],
-    //     ['400', 480],
-    //     ['500', 202],
-    //   ]);
-
-    // Set the options for the chart to be drawn.  This include the
-    // width, height, title, horizontal axis, vertical axis.  Finally
-    // turn off the legend.
-    var options = {
-        width: 700,
-        height: 400,
-	title: 'Number of Students Reached by Course Level',
-        hAxis: {
-            title: 'Course Level',
-            gridlines: {count: 5}
-        },
-        vAxis: {
-            title: 'Number of Students Reached'
-        },
-	legend: {
-	    position: 'none'
-	}
+// Set the options for the chart to be drawn.  This include the
+// width, height, title, horizontal axis, vertical axis.  Finally
+// turn off the legend.
+var options = {
+    width: 700,
+    height: 400,
+    title: 'Number of Students Reached by Course Level',
+    hAxis: {
+	title: 'Course Level',
+	gridlines: {count: 5}
+    },
+    vAxis: {
+	title: 'Number of Students Reached'
+    },
+    legend: {
+	position: 'none'
+    }
 };
 
 
 
 function vizInit() {
-// Create a new viz object using the google API -- specifically,
-// we are going to make a column chart inside the div called ex0
-// in the html file.
-chart = new google.visualization.ColumnChart(document.getElementById('ex0'));
+
+    // Must wait until the google library is loaded before we
+    // can actually instatiate an object of the DataTable type.
+    data = new google.visualization.DataTable();
+
+    // Store the data by creating a google DataTable object with
+    // two columns: Month and People Hours.
+    data.addColumn('string', 'Course Level');
+    data.addColumn('number', 'Number of Students');
+
+    // Add 12 rows to the DataTable, January - December of
+    // 2014.
+    data.addRows([
+        ['100', 1760],
+        ['200', 2120],
+        ['300', 698],
+        ['400', 480],
+        ['500', 202],
+      ]);
+
+    // Create a new viz object using the google API -- specifically,
+    // we are going to make a column chart inside the div called ex0
+    // in the html file.
+    chart = new google.visualization.ColumnChart(document.getElementById('ex0'));
 
 
+    // 9/19/2015 Corrected typo
+    // Make the initial query to get the whole Fusion table. The Fusion
+    // table’s ID is listed in red.
+    var query = "SELECT Month, Year, AY, Sessions FROM 1b7tW8h_6l35B2Pj7DCwfjHjurWzRiI-KpuvHnOgb&pli=1#rows:id=1";
 
-// Make the initial query to get the whole Fusion table. The Fusion
-// table’s ID is listed in red.
-var query = "SELECT Year, Course Level, Number of Students, AY, Sessions FROM 1b7tW8h_6l35B2Pj7DCwfjHjurWzRiI-KpuvHnOgb";
-
-var opts = {sendMethod: 'auto'};
-var queryObj = new google.visualization.Query('https://www.google.com/fusiontables/DataSource?docid=1b7tW8h_6l35B2Pj7DCwfjHjurWzRiI-KpuvHnOgb&pli=1#rows:id=1', opts);
-
-
-// Send the query and handle the response by logging the data
-// to the console.
-queryObj.setQuery(query);
-queryObj.send(function(e) {
-
-	data = e.getDataTable();
-
-    console.log(data);
-
-    // Create a view for academic year 2013-2014 that
-    // is the first two columns of the data, just the
-    // rows that have 2013-2014 for the value.
-
-    // First, get the textualized range of the year.
-	var thisYear = "" + year[0] + "-" + year[1];
-
-	// Next, create the object and get the rows
-	// corresponding to "thisYear".
-	views[thisYear] = new google.visualization.DataView(data);
-
-	views[thisYear].setRows(views[thisYear].getFilteredRows([{column: 2, value: thisYear}]));
-
-	// Get a subset of the columns.
-	views[thisYear].setColumns([0, 3]);
-
-	// Draw the chart for the initial academic year.
-	chart.draw(views[thisYear].toDataTable(), options);
+    var opts = {sendMethod: 'auto'};
+    var queryObj = new google.visualization.Query('https://www.google.com/fusiontables/DataSource?docid=1b7tW8h_6l35B2Pj7DCwfjHjurWzRiI-KpuvHnOgb&pli=1#rows:id=1', opts);
 
 
+    // Send the query and handle the response by logging the data
+    // to the console.
+    queryObj.setQuery(query);
+    queryObj.send(function(e) {
 
+	    data = e.getDataTable();
 
-});
+	    console.log(data);
+
+	    // Create a view for academic year 2013-2014 that
+	    // is the first two columns of the data, just the
+	    // rows that have 2013-2014 for the value.
+
+	    // First, get the textualized range of the year.
+	    var thisYear = "" + year[0] + "-" + year[1];
+
+	    // Next, create the object and get the rows
+	    // corresponding to "thisYear".
+	    views[thisYear] = new google.visualization.DataView(data);
+
+	    views[thisYear].setRows(views[thisYear].getFilteredRows([{column: 2, value: thisYear}]));
+
+	    // Get a subset of the columns.
+	    views[thisYear].setColumns([0, 3]);
+
+	    // Draw the chart for the initial academic year.
+	    chart.draw(views[thisYear].toDataTable(), options);
+
+	});
 }
 
 function vizController(thisYear) {
